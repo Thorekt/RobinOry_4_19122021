6985806dfdb4a8975d5c12f943267ad067458795
Robin Ory <35566455+Thorekt@users.noreply.github.com>
2022-01-07 17:31:28 +0100

    issue 2 corrigé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,5 +28,64 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// check if form is valid
+function validate() {
+  let isFormValid = true;
+  let checkboxs = [];
+  formData.forEach(element => {
+    element.querySelectorAll("input").forEach(input => {
+      if (input.name != "location") {
+        let isValid = isValidInput(input);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      } else {
+        checkboxs.push(input);
+      }
+    });
+  });
 
-console.log(formData);
+  if (!isOneLocationChecked(checkboxs)){
+    isFormValid = false;
+  }
+
+  return isFormValid;
+}
+
+function isValidInput(input) {
+  if ((input.id == "first" || input.id == "last") && input.value.length < 2) {
+    return false;
+  }
+  if (input.id == "quantity" && input.value.match(/^[0-9]+$/) == null) {
+    return false;
+  }
+
+  if (input.id == "email" && (!stringIsMail(input.value) && input.value.length == 0 )){
+    return false;
+  }
+  if (input.id == "checkbox1" && input.checked == false) {
+    return false;
+  }
+
+
+
+  return true;
+}
+
+function stringIsMail(string){
+  return string
+  .toLowerCase()
+  .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+}
+
+function isOneLocationChecked(checkboxs){
+  let isOneChecked = false;
+  checkboxs.forEach(checkbox => {
+    if(checkbox.checked){
+      isOneChecked = true;
+    }
+  });
+  return isOneChecked;
+}
